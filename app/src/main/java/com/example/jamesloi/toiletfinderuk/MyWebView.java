d000abc303d4c752ba1d73cd628b01e3c4802db2
jamesloi <james.loi@medicanimal.com>
2016-06-14 17:17:00 +0100

    adding location service

---
+++ app/src/main/java/com/example/jamesloi/toiletfinderuk/MyWebView.java
@@ -0,0 +1,8 @@
+package com.example.jamesloi.toiletfinderuk;
+
+/**
+ * Created by James.Loi on 14/06/2016.
+ */
+public class MyWebView {
+    public static android.webkit.WebView AndroidWebView;
+}
---
+++ app/src/main/java/com/example/jamesloi/toiletfinderuk/WebAppInterface.java
@@ -0,0 +1,7 @@
+package com.example.jamesloi.toiletfinderuk;
+
+/**
+ * Created by James.Loi on 14/06/2016.
+ */
+public class WebAppInterface {
+}
