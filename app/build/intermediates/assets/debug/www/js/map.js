e87ade0a86a373c003aa4dbae3c9e95f3f2fc0c2
jamesloi <james.loi@medicanimal.com>
2016-06-16 16:27:45 +0100

    map data

--- app/build/intermediates/assets/debug/www/js/map.js
+++ app/build/intermediates/assets/debug/www/js/map.js
@@ -1,13 +1,58 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var overPassApi = "https://www.overpass-api.de/api/interpreter?data=[out:json][timeout:60];node({slat},{slng},{nlat},{nlng})[amenity~toilets];out;";
+
+var mymap = L.map('mapid').setView([51.507448,-0.127776], 16);
+
+var toiletIcon = L.icon({
+                      iconUrl: 'js/images/toiletmarkerred.png',
+                      iconSize: [32, 37],
+                      iconAnchor: [16, 37],
+                      popupAnchor: [0, -28]
+                  });
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
     maxZoom: 18,
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.streets'
 }).addTo(mymap);
 
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+}).addTo(mymap);
 
-L.marker([51.5, -0.09]).addTo(mymap)
-    .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+mymap.on('click', function (e) {
+    var bounds = mymap.getBounds();
+    var boundsArray = bounds.toBBoxString();
+    var bbox = boundsArray.split(',');
+    var api = overPassApi.replace('{slat}',bbox[1]).replace('{slng}',bbox[0]).replace('{nlat}',bbox[3]).replace('{nlng}',bbox[2]);
+    console.log(api);
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", api, false );
+    xmlHttp.send( null );
+    console.log(xmlHttp.responseText);    
+    var jsonResult = xmlHttp.responseText;
+  L.geoJson(jsonResult, {
+      pointToLayer: function (feature, latlng) {
+          return L.marker(latlng, {icon: toiletIcon});
+      },
+      onEachFeature: onEachFeature
+  }).addTo(mymap);
+});
+
+//Android.getLastLocation();
+
+function getLocation(longitude, latitude)
+{
+    mymap.panTo(new L.LatLng(latitude, longitude));
+    L.marker([latitude, longitude]).addTo(mymap).bindPopup("<b>You are here!</b>").openPopup();
+    mymap.setView([latitude,longitude], 16);
+}
+
+function onEachFeature(feature, layer) {
+    var popupContent = "<h2>Toilet</h2>";
+    if (feature.properties) {
+        for (var key in feature.properties) {
+          if (feature.properties.hasOwnProperty(key) && key !== "@id") {
+            popupContent += "<p><strong>" + key + ":</strong> " + feature.properties[key] + "</p>";
+          }
+        }
+    }
+    layer.bindPopup(popupContent);
+}
